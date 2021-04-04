fe4b02fcdc0b5293c1081045aae2e0070fa0e139
Sukresh Manda <calago2488@naymeo.com>
2021-04-04 14:02:04 +0530

    user manual

--- app.py
+++ app.py
@@ -76,9 +76,8 @@ class Temp:
 		self.root.rowconfigure(0, weight=1)
 		self.root.rowconfigure(1, weight=5)
 		self.root.mainloop()
-if __name__ = '__main__':
-	
-Temp()
+if __name__ == '__main__':
+	Temp()
 	
 	
 	
